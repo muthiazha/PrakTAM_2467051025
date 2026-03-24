c86751dc204024ed93f790b35b43da76fc9e8078
muthiazha <muthiazenot21@gmail.com>
2026-03-25 04:59:05 +0700

    Tugas LKP 5

--- app/src/main/java/com/example/praktam_2467051025/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2467051025/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -13,40 +14,39 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.shape.RoundedCornerShape
 import com.example.praktam_2467051025.model.TagihanItem
 import com.example.praktam_2467051025.model.TagihanSource
 import com.example.praktam_2467051025.ui.theme.PrakTAM_2467051025Theme
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,29 +62,80 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DaftarTagihanScreen() {
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .statusBarsPadding(),
+        contentPadding = PaddingValues(24.dp),
+        verticalArrangement = Arrangement.spacedBy(24.dp)
     ) {
-        Text(
-            text = "Split Bill",
-            fontSize = 28.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Text(
-            text = "Pilih item untuk dibagi tagihannya",
-            style = MaterialTheme.typography.bodyMedium
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            contentPadding = PaddingValues(4.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(TagihanSource.dummyTagihan) { item ->
-                DetailTagihanItem(item = item)
+        item {
+            Text(
+                text = "Split Bill",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Pilih item untuk dibagi tagihannya",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Item Populer",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            LazyRow(
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(TagihanSource.dummyTagihan) { item ->
+                    TagihanRowItem(item = item)
+                }
+            }
+            Spacer(modifier = Modifier.height(45.dp))
+            Text(
+                text = "Semua Item Tagihan",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+        }
+        items(TagihanSource.dummyTagihan.sortedByDescending {
+            it.nama == "Sushi Date" || it.nama == "Spaghetti"
+        }) { item ->
+            DetailTagihanItem(item = item)
+        }
+    }
+}
+
+@Composable
+fun TagihanRowItem(item: TagihanItem) {
+    Card(
+        modifier = Modifier.width(160.dp),
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column {
+            Image(
+                painter = painterResource(id = item.imageRes),
+                contentDescription = item.nama,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp),
+                contentScale = ContentScale.Crop
+            )
+            Column(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = item.nama,
+                    style = MaterialTheme.typography.titleSmall,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Rp ${item.harga}",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.primary
+                )
             }
         }
     }
@@ -96,17 +147,20 @@ fun DetailTagihanItem(item: TagihanItem) {
 
     Card(
         modifier = Modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(12.dp),
-        elevation = CardDefaults.cardElevation(4.dp)
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        )
     ) {
-        Column {
+        Column(modifier = Modifier.fillMaxWidth()) {
             Box {
                 Image(
                     painter = painterResource(id = item.imageRes),
                     contentDescription = item.nama,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(120.dp),
+                        .height(200.dp),
                     contentScale = ContentScale.Crop
                 )
                 IconButton(
@@ -122,28 +176,30 @@ fun DetailTagihanItem(item: TagihanItem) {
                     )
                 }
             }
-            Column(modifier = Modifier.padding(8.dp)) {
-                Text(
-                    text = item.nama,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 14.sp
-                )
-                Text(
-                    text = item.deskripsi,
-                    fontSize = 12.sp
-                )
-                Text(
-                    text = "Rp ${item.harga}",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 12.sp
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Button(
-                    onClick = { },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("Tambah ke Tagihan", fontSize = 12.sp)
-                }
+
+            Column(modifier = Modifier.fillMaxWidth().padding(all = 16.dp)) {
+            Text(
+                text = item.nama,
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = item.deskripsi,
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Harga: Rp ${item.harga}",
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = { },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Tambah ke Tagihan")
+            }
             }
         }
     }
--- app/src/main/java/model/TagihanSource.kt
+++ app/src/main/java/model/TagihanSource.kt
@@ -4,9 +4,9 @@ import com.example.praktam_2467051025.R
 
 object TagihanSource {
     val dummyTagihan = listOf(
-        TagihanItem("Waffle", "ice cream waffle", 56000, R.drawable.waffle),
-        TagihanItem("Donat", "donat premium", 60000, R.drawable.donat),
-        TagihanItem("Sushi Date", "salmon sashimi", 93000, R.drawable.sushidate),
         TagihanItem("Spaghetti", "spaghetti bolognyes", 70000, R.drawable.spaghetti),
+        TagihanItem("Sushi date", "salmon sashimi", 93000, R.drawable.sushidate),
+        TagihanItem("Donat", "donat premium", 60000, R.drawable.donat),
+        TagihanItem("Waffle", "ice cream waffle", 56000, R.drawable.waffle),
     )
 }
